010c542aa591cfd9e2055a63ee827cfd773dd6c4
alpinus4 <alpinus4@gmail.com>
2017-12-16 01:16:26 +0100

    Cosmetic changes

--- correct-values.js
+++ correct-values.js
@@ -1,10 +1,6 @@
 
 
 window.onload = function(){
-
-};
-
-window.addEventListener('WebComponentsReady', function(e) {
   console.log("start");
   document.getElementById("birthdate").value=document.getElementById("birthdate").value.substring(0,10);
-});
+};
