e0d0d6f91b8ca9a7d611f0d09532b3c9fdd83dd0
alpinus4 <alpinus4@gmail.com>
2017-12-16 20:34:55 +0100

    File structure correction

--- correct-values.js
+++
@@ -1,8 +0,0 @@
-
-
-    /*window.addEventListener('WebComponentsReady', function (event) {
-      var x=this.$('#patient-demographics');
-      var y =  document.querySelector('#patient-demographics');
-      //y.correct();     NOT WORKING "TypeError: y.correct is not a function"
-      console.log(y); //returns correct value
-    });*/
