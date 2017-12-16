8284df9799176ed65f9264788cd3f1c77f4b5d08
alpinus4 <alpinus4@gmail.com>
2017-12-16 20:33:05 +0100

    Added birth date parsing and gender correction

--- correct-values.js
+++ correct-values.js
@@ -1,6 +1,8 @@
 
 
-window.onload = function(){
-  console.log("start");
-  document.getElementById("birthdate").value=document.getElementById("birthdate").value.substring(0,10);
-};
+    /*window.addEventListener('WebComponentsReady', function (event) {
+      var x=this.$('#patient-demographics');
+      var y =  document.querySelector('#patient-demographics');
+      //y.correct();     NOT WORKING "TypeError: y.correct is not a function"
+      console.log(y); //returns correct value
+    });*/
