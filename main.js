f044155bd4a9ea3e5c891ef4c11689b112bf324a
alpinus4 <alpinus4@gmail.com>
2017-12-27 00:59:07 +0100

    Added better look

---
+++ main.js
@@ -0,0 +1,82 @@
+var newsq="";
+var activePatientBtn, theSameBtn, currentPage;
+
+function OpenDashboard(patientBtn){
+  changeButtonActive(patientBtn);
+  putContentAndShow();
+}
+
+function changeButtonActive(patientBtn){
+  if(activePatientBtn){
+    if(activePatientBtn.id == patientBtn.id){
+      activePatientBtn.classList.toggle("active");
+      theSameBtn = true;
+    }else{
+      activePatientBtn.classList.remove("active");
+      activePatientBtn = patientBtn;
+      activePatientBtn.classList.add("active");
+    }
+  }else{
+    activePatientBtn = patientBtn;
+    activePatientBtn.classList.add("active");
+  }
+}
+
+function putContentAndShow(){
+  var componentSection = document.getElementById("componentSection");
+  var dashboard = document.getElementById("patientDashboard");
+  setCheckCurrentPage()
+
+  if(theSameBtn && !activePatientBtn.classList.contains("active")){
+    componentSection.innerHTML = "";
+    dashboard.style.display = "none";
+  }else{
+    componentSection.innerHTML = currentPage;
+    dashboard.style.display = "block";
+  }
+}
+
+function setCheckCurrentPage(){
+  var demographics = document.getElementById("demographicsPageBtn");
+  var visits = document.getElementById("visitsPageBtn");
+
+  if(demographics.classList.contains("active")){
+    currentPage = "<patient-demographics uuid='"+activePatientBtn.id+"'></patient-demographics>";
+    return "demographics";
+  }else if(visits.classList.contains("active")){
+    currentPage = "<last-visits patientuuid='"+activePatientBtn.id+"'></last-visits>";
+    return "visits";
+  }else{
+    demographics.classList.add("active");
+    setCheckCurrentPage()
+  }
+}
+
+
+
+function NewSearchQuery(){
+  newsq = document.getElementById("searchQuery").value;
+
+  if(newsq != document.getElementById("searchResults").sq){
+    if(activePatientBtn){
+      activePatientBtn.classList.remove("active");
+      activePatientBtn = null;
+    }
+    document.getElementById("searchResults").sq = newsq;
+  }
+}
+
+
+function activePageBtn(btn){
+  removeActiveFromOthers(btn);
+  btn.classList.add("active");
+  putContentAndShow();
+}
+
+function removeActiveFromOthers(btn){
+  if(btn.id == "demographicsPageBtn"){
+    document.getElementById("visitsPageBtn").classList.remove("active");
+  }else if(btn.id == "visitsPageBtn"){
+    document.getElementById("demographicsPageBtn").classList.remove("active");
+  }
+}
